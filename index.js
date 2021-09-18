df10bbb43c80fd3129bc35892f979059d34b022e
Thiago Santana <thiago.2.santana@atos.net>
2021-09-18 16:59:33 -0300

    feat: splash screen and app icon (android & ios)

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 
 import { AppRegistry } from 'react-native'
 import App from './App'
-import { name as appName } from './app.json'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 
-AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App))
+AppRegistry.registerComponent('NeumuMobile', () => gestureHandlerRootHOC(App))
