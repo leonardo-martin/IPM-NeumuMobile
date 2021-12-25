e5ed03e98e109c451b9e4e758bdc1486792ded77
Thiago Santana <thiago.2.santana@atos.net>
2021-12-25 16:32:35 -0300

    fix: build android and ios for react v0.66

--- src/types/constants/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@constants' {
-    export const _DEFAULT_FORMAT_DATE = 'dd-MM-YYYY'
-    export const _DEFAULT_FORMAT_TIME = 'HH:mm'
-    export const _DEFAULT_FORMAT_DATETIME = 'dd-MM-YYYY HH:mm:ss'
-}
