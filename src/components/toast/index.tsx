82f0d587649f26d9f356d853c1b33fb3774bfadf
Thiago Santana <thiago.2.santana@atos.net>
2021-12-26 10:35:22 -0300

    feat: toast component

---
+++ src/components/toast/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+import { ToastAndroid } from 'react-native'
+
+interface ToastProps {
+    visible: boolean
+    message: string
+    duration?: ToastAndroid["LONG"] | ToastAndroid["SHORT"]
+    gravity?: ToastAndroid["BOTTOM"] | ToastAndroid["TOP"] | ToastAndroid["CENTER"]
+    xOffset?: number
+    yOffset?: number
+}
+
+const Toast: FC<ToastProps> = ({ visible, message, duration, gravity, xOffset, yOffset }) => {
+
+    if (visible) {
+        ToastAndroid.showWithGravityAndOffset(
+            message,
+            Number(duration),
+            Number(gravity),
+            Number(xOffset),
+            Number(yOffset)
+        )
+        return null
+    }
+    return null
+}
+
+Toast.defaultProps = {
+    visible: false,
+    message: 'Message Example',
+    duration: ToastAndroid["LONG"],
+    gravity: ToastAndroid["BOTTOM"],
+    xOffset: 25,
+    yOffset: 50
+}
+
+export default Toast
