3ff636d744d4df2c1c9876f51811c2db4f575c6c
Thiago Santana <thiago.2.santana@atos.net>
2022-01-07 10:10:09 -0300

    fix: adding autocomplete component properties

--- src/components/autoComplete/index.tsx
+++ src/components/autoComplete/index.tsx
@@ -1,46 +1,18 @@
 import React, { FC, ReactElement } from 'react'
-import { KeyboardTypeOptions, StyleProp, TextStyle } from 'react-native'
-import { Autocomplete, Icon } from '@ui-kitten/components'
+import { Autocomplete, AutocompleteProps } from '@ui-kitten/components'
 import { City, UF } from '@models/Places'
 
-type AutoCompleteProps = {
+interface AutoCompleteProps extends AutocompleteProps {
     data: UF[] | City[] | string[]
-    placeholder?: string
-    label?: string
-    value?: string | undefined
-    onSelect?: (index: number) => void | undefined
-    onChangeText?: (text: string) => void | undefined
     renderOption: (item: any, index: number) => JSX.Element
-    clearInput: (field?: any) => void
-    disabled?: boolean
-    keyboardType?: KeyboardTypeOptions
-    maxLength?: number | undefined
-    autoCapitalize?: "none" | "characters" | "sentences" | "words" | undefined
-    style?: StyleProp<TextStyle>
 }
 
-const AutoCompleteComponent: FC<AutoCompleteProps> = ({
-    style, data, keyboardType, placeholder, maxLength, label, disabled, autoCapitalize, onSelect, onChangeText, renderOption, clearInput, value
-}): ReactElement => {
-
-    const renderRightIcon = (props: any) => (
-        <Icon {...props} name='close-outline' pack='ionicons' onPress={clearInput} />
-    )
+const AutoCompleteComponent: FC<AutoCompleteProps> = (props): ReactElement => {
 
     return (
         <Autocomplete
-            style={style}
-            label={label}
-            keyboardType={keyboardType}
-            placeholder={placeholder}
-            value={value}
-            autoCapitalize={autoCapitalize}
-            disabled={disabled}
-            maxLength={maxLength}
-            accessoryRight={value !== '' ? renderRightIcon : undefined}
-            onChangeText={onChangeText}
-            onSelect={onSelect}>
-            {data.map(renderOption)}
+            {...props}   >
+            {props.data.map(props.renderOption)}
         </Autocomplete>
     )
 }
