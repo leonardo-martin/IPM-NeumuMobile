fe76b71a4eaa95fa4d7d32fa5c703d4525cd1d34
Thiago Santana <thiago.2.santana@atos.net>
2022-01-13 11:31:00 -0300

    fix: changed modalize component

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -7,6 +7,8 @@ import { Text, Card, Icon, useStyleSheet } from '@ui-kitten/components'
 import { SafeAreaLayout } from '@components/safeAreaLayout'
 import { useFocusEffect } from '@react-navigation/native'
 import { dashboardStyle } from './style'
+import ModalizeFixed from '@components/modalize'
+import HeaderAdmin from 'components/header/admin'
 
 const DashboardScreen: FC<DrawerContentComponentProps> = ({
   navigation
@@ -38,84 +40,81 @@ const DashboardScreen: FC<DrawerContentComponentProps> = ({
   )
 
   return (
-    <SafeAreaLayout insets='bottom' level='1' style={styles.safeArea}>
-      <View style={styles.content}>
-        <StatusBar barStyle="dark-content" backgroundColor='transparent' translucent />
-        <View style={styles.cardContainer}>
-          <Text category="h5" status='basic' style={styles.text}>
-            Como podemos te ajudar?
-          </Text>
-          <View style={styles.cardGroupPrimary}>
-            <Card onPress={gotToSchedule}>
-              <View style={styles.cardDefault}>
-                <Icon style={styles.iconOrange} name="calendar-outline" size={50} pack='ionicons' />
-                <Text category="h6" style={[styles.cardText, {
-                  marginHorizontal: -8, flex: 1
-                }]}>
-                  Quero agendar uma consulta
+    <>
+      <HeaderAdmin />
+      <SafeAreaLayout insets='bottom' level='1' style={styles.safeArea}>
+        <View style={styles.content}>
+          <StatusBar barStyle="dark-content" backgroundColor='transparent' translucent />
+          <View style={styles.cardContainer}>
+            <Text category="h5" status='basic' style={styles.text}>
+              Como podemos te ajudar?
+            </Text>
+            <View style={styles.cardGroupPrimary}>
+              <Card onPress={gotToSchedule}>
+                <View style={styles.cardDefault}>
+                  <Icon style={styles.iconOrange} name="calendar-outline" size={50} pack='ionicons' />
+                  <Text category="h6" style={[styles.cardText, {
+                    marginHorizontal: -8, flex: 1
+                  }]}>
+                    Quero agendar uma consulta
+                  </Text>
+                </View>
+              </Card>
+            </View>
+            <View style={styles.cardGroupSecondary}>
+              <Card style={styles.card} onPress={gotToProfile}>
+                <View style={styles.cardDefault}>
+                  <Icon style={styles.iconPrimary} name='prescription' size={40} pack='fontisto' />
+                </View>
+                <Text category="h6" style={styles.cardText}>
+                  Meu perfil
                 </Text>
-              </View>
-            </Card>
-          </View>
-          <View style={styles.cardGroupSecondary}>
-            <Card style={styles.card} onPress={gotToProfile}>
-              <View style={styles.cardDefault}>
-                <Icon style={styles.iconPrimary} name='prescription' size={40} pack='fontisto' />
-              </View>
-              <Text category="h6" style={styles.cardText}>
-                Meu perfil
-              </Text>
-            </Card>
-            <Card style={styles.card} onPress={gotToAppointments}>
-              <View style={styles.cardDefault}>
-                <Icon style={styles.iconPrimary} name='stethoscope' size={40} pack='font-awesome' />
-              </View>
-              <Text category="h6" style={styles.cardText}>
-                Minhas consultas
-              </Text>
-            </Card>
-          </View>
-          <View style={styles.cardGroupSecondary}>
-            <Card style={styles.card}>
-              <View style={styles.cardDefault}>
-                <Icon
-                  style={styles.iconPrimary}
-                  name="information-circle-outline"
-                  size={40}
-                  pack='ionicons'
-                />
-              </View>
-              <Text category="h6" style={styles.cardText}>
-                Sobre
-              </Text>
-            </Card>
-            <Card style={styles.card}>
-              <View style={styles.cardDefault}>
-                <Icon style={styles.iconPrimary} name="help-circle-outline" size={40} pack='ionicons' />
-              </View>
-              <Text category="h6" style={styles.cardText}>
-                Ajuda
-              </Text>
-            </Card>
+              </Card>
+              <Card style={styles.card} onPress={gotToAppointments}>
+                <View style={styles.cardDefault}>
+                  <Icon style={styles.iconPrimary} name='stethoscope' size={40} pack='font-awesome' />
+                </View>
+                <Text category="h6" style={styles.cardText}>
+                  Minhas consultas
+                </Text>
+              </Card>
+            </View>
+            <View style={styles.cardGroupSecondary}>
+              <Card style={styles.card}>
+                <View style={styles.cardDefault}>
+                  <Icon
+                    style={styles.iconPrimary}
+                    name="information-circle-outline"
+                    size={40}
+                    pack='ionicons'
+                  />
+                </View>
+                <Text category="h6" style={styles.cardText}>
+                  Sobre
+                </Text>
+              </Card>
+              <Card style={styles.card}>
+                <View style={styles.cardDefault}>
+                  <Icon style={styles.iconPrimary} name="help-circle-outline" size={40} pack='ionicons' />
+                </View>
+                <Text category="h6" style={styles.cardText}>
+                  Ajuda
+                </Text>
+              </Card>
+            </View>
           </View>
         </View>
-      </View>
-
-      <Modalize ref={modalizeRef} snapPoint={300} adjustToContentHeight={true}>
-        <SafeAreaLayout level='2' style={styles.safeAreaModalize}>
-          <View style={styles.contentModalize}>
-            <Text style={styles.textConfirmExit}>Deseja realmente sair do aplicativo?</Text>
-            <TouchableOpacity style={styles.contentButton} activeOpacity={0.75} onPress={exitApp}>
-              <Text style={styles.contentButtonText}>{'Sim'.toUpperCase()}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.contentButton, styles.buttonOutline]} activeOpacity={0.75} onPress={handleClose}>
-              <Text style={[styles.contentButtonText, styles.buttonTextOutline]}>{'Não'.toUpperCase()}</Text>
-            </TouchableOpacity>
-          </View>
-        </SafeAreaLayout>
-      </Modalize>
-
-    </SafeAreaLayout>
+      </SafeAreaLayout>
+      <ModalizeFixed ref={modalizeRef} snapPoint={300} adjustToContentHeight={true} >
+        <Text style={styles.textConfirmExit}>Deseja realmente sair do aplicativo?</Text>
+        <TouchableOpacity style={styles.contentButton} activeOpacity={0.75} onPress={exitApp}>
+          <Text style={styles.contentButtonText}>{'Sim'.toUpperCase()}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.contentButton, styles.buttonOutline]} activeOpacity={0.75} onPress={handleClose}>
+          <Text style={[styles.contentButtonText, styles.buttonTextOutline]}>{'Não'.toUpperCase()}</Text>
+        </TouchableOpacity>
+      </ModalizeFixed>
+    </>
   )
 }
 
